8ab0f702c6740ba76fe3b97954f0c400bc07a60a
Florent Guillaume <fg@nuxeo.com>
2013-07-11 17:24:18 +0200

    NXP-9709: port to 5.6.0

--- src/main/java/org/nuxeo/ecm/core/storage/sql/UnifiedCachingMapper.java
+++ src/main/java/org/nuxeo/ecm/core/storage/sql/UnifiedCachingMapper.java
@@ -124,8 +124,8 @@ public class UnifiedCachingMapper extends UnifiedCachingRowMapper implements
     }
 
     @Override
-    public void createClusterNode() throws StorageException {
-        mapper.createClusterNode();
+    public String createClusterNode() throws StorageException {
+        return mapper.createClusterNode();
     }
 
     @Override
@@ -134,14 +134,15 @@ public class UnifiedCachingMapper extends UnifiedCachingRowMapper implements
     }
 
     @Override
-    public void insertClusterInvalidations(Invalidations invalidations)
-            throws StorageException {
-        mapper.insertClusterInvalidations(invalidations);
+    public void insertClusterInvalidations(Invalidations invalidations,
+            String nodeId) throws StorageException {
+        mapper.insertClusterInvalidations(invalidations, nodeId);
     }
 
     @Override
-    public Invalidations getClusterInvalidations() throws StorageException {
-        return mapper.getClusterInvalidations();
+    public Invalidations getClusterInvalidations(String nodeId)
+            throws StorageException {
+        return mapper.getClusterInvalidations(nodeId);
     }
 
     @Override
