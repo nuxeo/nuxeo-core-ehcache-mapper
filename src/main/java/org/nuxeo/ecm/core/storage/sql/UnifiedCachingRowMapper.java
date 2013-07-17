12375b60a8eb19dc860ab78b25446387fd131545
Florent Guillaume <fg@nuxeo.com>
2013-07-17 18:23:04 +0200

    NXP-9709: work also with non-XA mode

--- src/main/java/org/nuxeo/ecm/core/storage/sql/UnifiedCachingRowMapper.java
+++ src/main/java/org/nuxeo/ecm/core/storage/sql/UnifiedCachingRowMapper.java
@@ -236,7 +236,7 @@ public class UnifiedCachingRowMapper implements RowMapper {
     }
 
     protected boolean useEhCache() {
-        return isXA && hasTransaction();
+        return !isXA || hasTransaction();
     }
 
     protected void ehCachePut(Element element) {
